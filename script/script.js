0f79e21fbf5569a4aea0ea1fd377f340d189311a
Николай <Koniukowa@yandex.ru>
2021-01-27 11:41:04 +0700

    fix js

--- script/script.js
+++ script/script.js
@@ -1,22 +1,13 @@
 'use strict';
 
-const hamburger = document.querySelector('.hamburger');
 const headerNavWrap = document.querySelector('.header-nav-wrap');
-const cross = document.querySelector('.cross');
 
-function toggle() {
-    headerNavWrap.classList.toggle('d-none');
-    headerNavWrap.classList.toggle('d-flex');
-}
-
-hamburger.addEventListener('click', () => {
-    toggle();
-})
-
-headerNavWrap.addEventListener('click', elem => {
+document.addEventListener('click', elem => {
     const target = elem.target;
-    console.log(target)
-    if (target.closest('a') || target === cross) {
-        toggle();
+    if (target.closest('.link')
+        || target.closest('.cross')
+        || target.closest('.hamburger')) {
+        headerNavWrap.classList.toggle('d-none');
+        headerNavWrap.classList.toggle('d-flex');
     }
 })
